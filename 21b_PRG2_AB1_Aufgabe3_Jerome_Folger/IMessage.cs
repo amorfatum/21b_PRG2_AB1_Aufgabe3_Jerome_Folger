91d471b0a0f13de4d9b9b5fe83fb6507bbb7b5e1
folger-j <jerome.folger@gmail.com>
2023-02-10 11:38:32 +0100

    Add project files.

---
+++ 21b_PRG2_AB1_Aufgabe3_Jerome_Folger/IMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21b_PRG2_AB1_Aufgabe3_Jerome_Folger
+{
+    internal interface IMessage
+    {
+        string Message { get; set; }
+
+        void SendMessage(string message);
+
+        public string RecieveMessage(string message);
+
+        public void OutputMessage();
+    }
+}
---
+++ 21b_PRG2_AB1_Aufgabe3_Jerome_Folger/Sms.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21b_PRG2_AB1_Aufgabe3_Jerome_Folger
+{
+    internal class Sms : IMessage
+    {
+        public string Message { get; set; }
+        public void SendMessage(string message)
+        {
+            Message = message;
+        }
+
+        public string RecieveMessage(string message)
+        {
+            return Message;
+        }
+
+        public void OutputMessage()
+        {
+            Console.WriteLine(Message);
+        }
+    }
+}
